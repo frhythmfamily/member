08541fc8c545fa2e0d7d83cb5a6928e3de70eda3
frhythmfamily <frhythmfamily@gmail.com>
2024-01-15 16:57:14 +0700

    menerapkan alpine js

---
+++ src/app.js
@@ -0,0 +1,31 @@
+document.addEventListener('alpine:init', () => {
+    Alpine.data('members', () => ({
+        items: [
+            {id: 1, name: 'zan', img: 'zan.jpg'},
+            {id: 2, name: 'tama', img: 'tama.jpg'},
+            {id: 3, name: 'shaldan', img: 'shaldan.jpg'},
+            {id: 4, name: 'frenklyn', img: 'frenklyn.jpg'},
+            {id: 5, name: 'raihan', img: 'raihan.jpg'},
+            {id: 6, name: 'iqbal', img: 'iqbal.jpg'},
+            {id: 7, name: 'alica', img: 'alica.jpg'},
+            {id: 8, name: 'mita', img: 'mita.jpg'},
+            {id: 9, name: 'dinda', img: 'dinda.jpg'},
+            {id: 10, name: 'sarah', img: 'sarah.jpg'},
+            {id: 11, name: 'neska', img: 'neska.jpg'},
+            {id: 12, name: 'mina', img: 'mina.jpg'},
+            {id: 13, name: 'ericka', img: 'ericka.jpg'},
+            {id: 14, name: 'kezia', img: 'kezia.jpg'},
+            {id: 15, name: 'nazwa', img: 'nazwa.jpg'},
+            {id: 16, name: 'arra', img: 'arra.jpg'},
+            {id: 17, name: 'gesya', img: 'gesya.jpg'},
+            {id: 18, name: 'aqila', img: 'aqila.jpg'},
+            {id: 19, name: 'kyu', img: 'kyu.jpg'},
+            {id: 20, name: 'laura', img: 'laura.jpg'},
+            {id: 21, name: 'felycia', img: 'felycia.jpg'},
+            {id: 22, name: 'bernice', img: 'bernice.jpg'},
+            {id: 23, name: 'nadia', img: 'nadia.jpg'},
+            {id: 24, name: 'ervid', img: 'ervid.jpg'},
+            {id: 25, name: 'michelle', img: 'michelle.jpg'}
+        ]
+    }));
+});
