c82d90bb9a58ea55fba39c9098a8c6dc298e23c3
frhythmfamily <frhythmfamily@gmail.com>
2024-05-02 12:04:32 +0700

    update alpinejs

--- src/app.js
+++ src/app.js
@@ -1,3 +1,9 @@
+import Alpine from 'alpinejs';
+
+window.Alpine = Alpine;
+
+Alpine.start();
+
 document.addEventListener('alpine:init', () => {
     Alpine.data('members', () => ({
         items: [
