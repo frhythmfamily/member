b4a2c46d09c2c43cf9ecf767a197b1b88b380cf5
frhythmfamily <frhythmfamily@gmail.com>
2024-05-02 12:30:38 +0700

    balik ke awal alpine js

--- src/app.js
+++ src/app.js
@@ -1,9 +1,3 @@
-import Alpine from 'alpinejs';
-
-window.Alpine = Alpine;
-
-Alpine.start();
-
 document.addEventListener('alpine:init', () => {
     Alpine.data('members', () => ({
         items: [
